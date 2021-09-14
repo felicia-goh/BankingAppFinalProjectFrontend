34d298e93b76d0f1aef667e0d1849c7512cdcdd0
Hiew Jin Nee <hiewjinneeeee@gmail.com>
2021-09-14 10:27:24 +0800

    Users are able to login with valid credentials
    Session still doesn't work.

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import UserList from './components/user-list.component';
+import AccountList from './components/account-list.component';
+import LandingPage from './components/landing-page.component';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        {/* <UserList /> */}
+        {/* <AccountList /> */}
+        <LandingPage />
+      </div>
     </div>
   );
 }
---
+++ react-project/src/components/landing-page.component.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+import LoginDataService from "../services/login.service"
+
+export default function LandingPage() {
+
+    const [auth, setAuth] = useState({ email: '', login_password: '', isLoggedIn: false });
+    const [currUser, setCurrUser] = useState([]);   // id, name, type
+
+    // function setSessionID() {                                   // setSession() should be done when login
+    //     setCurrUser({ id: 20, name: 'jane', email: 'janedoe@gmail.com' });;
+    //     sessionStorage.setItem('mySession', JSON.stringify(currUser));
+    // }
+
+    useEffect(() => {
+
+    }, [])
+
+    function autheticateUser(e) {
+        e.preventDefault()
+        console.log(auth);
+        LoginDataService.login(auth)
+            .then(response => {
+                setCurrUser(response.data)
+                setAuth({ ...auth, email: '', login_password: '', isLoggedIn: true })
+                console.log("response: " + JSON.stringify(response));
+            })
+            .catch(e => {
+                console.log(e.detailedMsg);
+            });
+    }
+
+    return (
+        auth.isLoggedIn ?
+            <div>
+                <h4>Welcome back, {currUser.customer_name} !</h4>
+            </div>
+            :
+            <div>
+                <h2>Login</h2>
+                <form onSubmit={autheticateUser}>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Email</label>
+                        <input type="text" class="form-control" id="email" aria-describedby="emailHelp" value={auth.email} onChange={e => setAuth({ ...auth, email: e.target.value })} />
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" value={auth.login_password} onChange={e => setAuth({ ...auth, login_password: e.target.value })} />
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+    )
+}
