f94e0eeb52b64410295038ae46c6b7d877776604
Felicia Goh <minhui.felicia@gmail.com>
2021-09-14 10:36:25 +0800

    list transactions from account

---
+++ react-project/src/services/transaction.service.js
@@ -0,0 +1,30 @@
+import http from "../http-common";
+
+class TransactionDataService {
+    getAll() {
+        return http.get("/transactions");
+    }
+
+    get(account_id) {
+        return http.get(`/accounts/${account_id}/transactions`);
+    }
+
+    // create(data) {
+    //     return http.post(`/accounts/${account_id}/transactions/new`, data);
+    // }
+
+    // delete(id) {
+    //     return http.delete(`/users/${id}`);
+    // }
+
+    // deleteAll() {
+    //     return http.delete(`/users`);
+    // }
+
+    // findByName(name) {
+    //     return http.get(`/users/name/${name}`);
+    // }
+
+}
+
+export default new TransactionDataService();
